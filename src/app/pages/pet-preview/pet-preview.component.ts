2b1f2588c0440542af0023cf4bb07d6ffe252eb4
Caaribean <klaudia.gnatkiewicz@gmail.com>
2023-06-23 15:00:30 +0200

    Add section with photos of customers

--- src/app/pages/pet-preview/pet-preview.component.ts
+++ src/app/pages/pet-preview/pet-preview.component.ts
@@ -8,10 +8,11 @@ import { ActivatedRoute } from "@angular/router";
 })
 export class PetPreviewComponent {
     public id: string | null = '';
+
     constructor(private route: ActivatedRoute) { }
 
     ngOnInit(): void {
-        console.log(this.id);
+        console.log(this.id); 
         this.id = this.route.snapshot.paramMap.get('id');
         console.log(this.id);
         console.log(this.route)
